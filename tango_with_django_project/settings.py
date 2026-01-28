eafb1955e6c1104ba1408b90288cfff07668a66f
“Euan <2999713S@student.gla.ac.uk>
2026-01-28 02:13:17 +0000

    4.3 working

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -19,6 +19,8 @@ TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 
 STATIC_DIR = os.path.join(BASE_DIR, 'static')
 
+MEDIA_DIR = os.path.join(BASE_DIR, 'media')
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
@@ -124,3 +126,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [STATIC_DIR,]
+
+# Media files
+MEDIA_URL = '/media/'
+MEDIA_ROOT = MEDIA_DIR
