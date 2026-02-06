69787559650364cfd39cd3dee50ac3bda3f0112e
“Euan <2999713S@student.gla.ac.uk>
2026-02-06 22:24:58 +0000

    5.7 working

--- rango/models.py
+++ rango/models.py
@@ -4,8 +4,12 @@ from django.db import models
 class Category(models.Model):
     name = models.CharField(max_length=128, unique=True)
     
+    class Meta:
+        verbose_name_plural = 'Categories'
+
     def __str__(self):
         return self.name
+
 class Page(models.Model):
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     title = models.CharField(max_length=128)
@@ -13,4 +17,4 @@ class Page(models.Model):
     views = models.IntegerField(default=0)
     
     def __str__(self):
-        return self.titl
+        return self.title
