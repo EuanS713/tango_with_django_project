86575ce1e39be2b3aa2ad183897c6b0b360fa61a
“Euan <2999713S@student.gla.ac.uk>
2026-02-13 15:28:44 +0000

    DONE

--- rango/views.py
+++ rango/views.py
@@ -1,15 +1,12 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from rango.models import Category
-from rango.models import Page
-from rango.forms import CategoryForm
-from django.shortcuts import redirect
-from django.urls import reverse
-from rango.forms import PageForm
-from rango.forms import UserForm, UserProfileForm
-from django.contrib.auth.decorators import login_required
-from django.contrib.auth import authenticate, login, logout
 from datetime import datetime
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse
+from django.shortcuts import redirect, render
+from django.urls import reverse
+from rango.forms import CategoryForm, PageForm, UserForm, UserProfileForm
+from rango.models import Category, Page
+
 
 
 def index(request):
